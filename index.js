1f7b243b493944729f229409f10a5377da028bc1
zeikskurset <hideousdegenerate@yahoo.com>
2022-05-14 00:53:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,413 @@
+const Discord = require('discord.js')
+const axios = require('axios')
+const fs = require('fs')
+const config = require('./secret_key.json')
+
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]})
+
+const prefix = "!"
+
+const APIURL = "https://api.opendota.com/api/";
+
+const cacheFilename = "cache.json"
+
+let cacheLifetime = 300000;
+
+const cacheDumpInterval = 300000;
+
+const maxSimultFetch = 20;
+
+const maxCallsPerMinute = 50;  
+
+let queue = []
+
+let aliases = {}
+
+let cache = {}
+
+let discordAliases = {}
+
+let leaderboard = []
+
+let queueActive = false;
+
+let queueInterval;
+
+//[Utility]
+
+//checking if account id is valid
+function validAccId(id) {
+	return /^\d\d\d\d\d\d\d\d\d$/.test(id)
+}
+
+//defining criteriae and timelimits and transforming them into numbers
+function getParamIndices(criteria, timelimit) {
+	let criteriae = ["wins", "games", "winrate", "net", "gpm", "xpm", "kills", "deaths", "kda", "leaver", "damage"]
+  let timelimits = ["alltime", "recent", "today"]
+  let cInd = criteriae.indexOf(criteria)
+	let tInd = timelimits.indexOf(timelimit)
+	return {cInd: cInd, tInd: tInd}
+}
+
+//fns for getting the proper stat from WL info
+let wlfns = [
+	//Wins
+	(a) => {
+		a.mainCrit = a.json.win
+	},
+	//Total games
+	(a) => {
+		a.mainCrit = a.json.win + a.json.lose
+	},
+	//Winrate
+	(a) => {
+		a.mainCrit = a.json.win / (a.json.lose + a.json.win) * 100
+	}
+]
+
+
+//[Cache]
+
+function dumpCache(){
+	fs.writeFile(cacheFilename, JSON.stringify(cache), (err) => {
+		if (err)
+			console.log(err)
+	})
+}
+
+function loadLatestCache() {
+	fs.readFile(cacheFilename, 'utf8', function (err,data) {
+  	if (err) {
+  		console.log(err);
+  	}
+  	cache = JSON.parse(data);
+	});
+}
+
+// load cache on starting the bot if cacheFilename is provided earlier
+if (cacheFilename)
+	loadLatestCache();
+
+// start dumping cache periodically after some time
+setTimeout(() => {
+	setInterval(() => {
+		dumpCache();
+		console.log("Dumping cache")
+	}, cacheDumpInterval)
+}, cacheDumpInterval)
+
+
+
+//[Calls to API and related stuff]
+
+// gets a url extension to the API; checks for cache for such a request and either fetches a reques
+// or gets it from memory. returns a promise
+
+async function fetch(urlextension, forceupdate=false){
+	console.log("Geting " + urlextension)
+	console.log("Forced update: " + forceupdate)
+
+	let noCache = cache[urlextension] == undefined
+
+	if (noCache)
+		console.log("No cache found")
+
+	cacheExpired = false;
+
+	if (!noCache) {
+		cacheExpired =  !cache[urlextension].permanent && ((Date.now() - cache[urlextension].timestamp) > cacheLifetime)
+		if (cacheExpired)
+			console.log("Cache expired")
+	}
+
+	permanent = /^matches/.test(urlextension)
+
+	if (forceupdate || noCache || cacheExpired) {
+		console.log("Requesting " + urlextension + "...")
+		let result = await axios.get(APIURL + urlextension)
+		console.log("Done " + urlextension)
+		if (result.status != 200) {
+			console.log("Fetch failed: " + result.status)
+			throw new Error();
+		}
+		
+  	let json =  result.data;
+  	
+  	if (permanent) 
+  		console.log("Caching " + urlextension + " permanently")
+  	
+  	cache[urlextension] = {
+  		timestamp: Date.now(),
+  		data: json,
+  		permanent: permanent
+  	}
+	}
+
+	return cache[urlextension].data
+}
+
+//grabs a bunch of games. returns a promise
+async function getMatches(matchIds) {
+	if (matchIds.length > maxSimultFetch) {
+		console.log("Attempted fetching too many requests")
+		throw new Error()
+		return;
+	}
+
+	let promises = []
+	let results = []
+
+	matchIds.forEach((matchId)=>{
+		promises.push(fetch("matches/"+matchId.match_id).then((res)=>{
+			results.push(res)
+		}))
+	})
+
+	await Promise.all(promises).then((res) => {
+		console.log("Collected " + matchIds	+ " matches")
+	});
+
+	return results
+}
+
+//I'll probably have to rework this later
+async function getLeaderboard(cInd, tInd) {
+  let urlext;
+
+	let results = []
+	let promises = []
+
+	leaderboard.forEach((user) => {
+		if (cInd < 3) {
+			urlext = "players/" + aliases[user] + "/wl"
+		} 
+		if (cInd > 2) {
+			urlext = "players/" + aliases[user] + "/matches"
+		}
+		if (tInd == 1) {
+			urlext += "?limit=20"
+		}
+		if (tInd ==2) {
+			urltext += "?date=1"
+		}
+		let newEntry = {alias: user}
+		results.push(newEntry)
+		promises.push(fetch(urlext).then((res)=>{
+			newEntry.json = res
+		}))
+	})
+
+	if (cInd < 3){
+		await Promise.all(promises).then(() => {
+			console.log("Applying functions for WL stats...")
+			results.every(wlfns[cInd]);
+		});
+	}
+
+	console.log("exiting getLeaderboard")
+	console.log(results)
+	return results
+}
+
+// grabs a part of game caching queue and requests it for caching. !!! Doesnt check
+// whether a game is already cached, so that should be fixed
+
+function fetchBatchFromQueue(){
+	let batch = queue.length > maxCallsPerMinute ? queue.slice(0, maxCallsPerMinute) : queue
+
+	queue = queue.slice(batch.length)
+
+	if(queue.length === 0) {
+		setTimeout(() => {
+			queueActive = false;
+			clearInterval(queueInterval);
+		}, 60000)
+	}
+
+	console.log("Fetching " + batch.length + " games")
+
+	batch.forEach((game) => {
+		fetch("matches/"+game.match_id)
+	})
+}
+
+//[Getting info in advance]
+
+let heroes = fetch("heroes");
+
+let info;
+
+fs.readFile('./commands.txt', 'utf8', function (err,data) {
+  if (err) {
+  	return console.log(err);
+  }
+  info = data;
+});
+
+
+//[Message handling]
+
+
+client.on("messageCreate", async function (message) {
+
+  function getUsersAlias() {
+  	return discordAliases[message.member.user.id] ? discordAliases[message.member.user.id].alias : undefined
+  }
+
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "commands" || command === "help") {
+  	message.reply(info)
+  }
+
+  if (command === "deletecache" || command === "dc") {
+  	cache = {}
+  	message.reply("Кэш очищен");
+  }
+
+  if (command === "cachelifetime") {
+  	cacheLifetime = args.length > 0 ? args[0] : cacheLifetime
+  	message.reply("Кэш хранится " + cacheLifetime + "мс")
+  }
+
+  if (command === "iam"){
+  	if (args.length === 0) {
+  		if (discordAliases[message.member.user.id] != undefined) {
+  			message.reply("Вы - " + discordAliases[message.member.user.id].alias)
+  		} else {
+  			message.reply("У вас не привязан алиас")
+  		}
+  	} else {
+  		if(aliases[args[0]] == undefined) {
+  			message.reply("Такого алиаса нет")
+  			return
+  		}
+  		discordAliases[message.member.user.id] = {alias: args[0], name: message.member.user.username}
+  		message.reply("Вы теперь - " + args[0])
+  	}
+  }
+
+  if (command === "whois") {
+  	if (args.length === 0) {
+  		message.reply("Нужно указать алиас")
+  		return
+  	}
+
+	let found = false;
+	let res;
+
+  	Object.entries(discordAliases).forEach(([userid, data]) => {
+  		if(data.alias === args[0]) {
+  			found = true;
+  			res = data.name;
+  		}
+  	})
+
+  	if (!found) {
+  		message.reply("Этот алиас не привязан")
+  		return
+  	}
+
+  	message.reply(args[0] + " это " + res)
+  }
+
+  if (command === "forgetme") {
+  	discordAliases[message.member.user.id] = undefined;
+  	message.reply("От вашего аккаунта отвязан алиас");
+  }
+
+
+  if (command === "alias" || command === "a") {
+  	if (args.length > 1) {
+
+  		if (validAccId(args[0])) {
+  			message.reply("Не удалось создать алиас " + args[0] + ", он может быть айди аккаунта")
+  			return
+  		} 
+  		if (!validAccId(args[1])) {
+  			message.reply(args[1] + " не является айди аккаунта")
+  			return
+  		}
+  		
+  		aliases[args[0]] = args[1];
+
+  		let msgtext = args[0] + " теперь алиас для айди " + args[1];
+
+  		if (args.length > 2 && (args[2] === "me")) {
+  			discordAliases[message.member.user.id] = {alias: args[0], name: message.member.user.username}
+  			msgtext += " привязанного к вашему аккаунту"
+  		}
+
+  		if (args.length > 3 && (args[3] === "enter")) {
+  			leaderboard.push(args[0])
+  			msgtext += " и участвующий в лидерборде"
+  		}
+
+  		message.reply(msgtext)
+  		
+  	} else message.reply("Нужно указать алиас и айди")
+  }
+
+  if (command === "aliases") {
+  	message.reply(JSON.stringify(discordAliases))
+  }
+
+  //[Network-related commands]
+
+  try {
+
+  	if (command === "cachegames") {
+  		let alias = args.length > 0 ? args[0] : getUsersAlias();
+  		if (aliases[alias] == undefined) {
+  			message.reply("Такого алиаса нет")
+  			return
+  		}
+  		fetch("players/"+aliases[alias]+"/matches").then((res)=>{
+  			queue.push(...res);
+  			if (!queueActive) {
+  				queueInterval = setInterval(fetchBatchFromQueue, 60000);
+  				fetchBatchFromQueue();
+  				queueActive = true;
+  			}
+  			message.reply("Начали сохранять данные о " + res.length + "играх");
+  		})
+  	}
+
+  	if (queue.length === 0) {
+	  	if (command === "leaderboard" || command === "all") {
+	 	  	let paramIndices = getParamIndices(args[0], args[1])
+	 			getLeaderboard(paramIndices.cInd, paramIndices.tInd).then((results) => {
+	 				console.log("Replying")
+	 				console.log(results)
+	 				message.reply(JSON.stringify(results))	
+	 			}); 	  	
+	  	}
+  	}	else {
+  		message.reply("В данный момент бот прогружает игры и не может выполнять ряд команд.");
+  	}
+	} catch (error) {
+		console.log(error)
+		message.reply("Упс, что-то пошло не так. Вероятно превышено число запросов к API")
+	}
+
+  if(command === "enterleaderboard" || command === "enter") {
+  	let alias = args.length > 0 ? args[0] : getUsersAlias();
+  	if (alias == undefined) {
+  		message.reply("Привяжите алиас или укажите его в команде")
+  		return
+  	}
+  	leaderboard.push(alias)
+  	message.reply("Вы теперь участник лидерборда")
+  }
+
+
+
+
+});
+
+client.login(config.secret_key)
