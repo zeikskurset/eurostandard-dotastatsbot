103f12a8c20f53b8a9f75deb6dc984e9cf90cd5b
zeikskurset <hideousdegenerate@yahoo.com>
2022-05-15 00:20:11 +0300

    first kinda working version

--- cache.js
+++ cache.js
@@ -5,22 +5,26 @@ const fs = require('fs')
 exports.data = {}
 
 exports.dump = function(){
+	console.log("Dumping cache")
 	fs.writeFile(config.cacheFilename, JSON.stringify(this.data), (err) => {
 		if (err)
 			console.log(err)
 	})
-
-	console.log("Dumping cache")
 }
 
 exports.loadLatest = function(){
+	console.log("Loading cache")
 	fs.readFile(config.cacheFilename, 'utf8', function (err,data) {
 			if (err) {
 				console.log(err);
 			}
-			this.data = JSON.parse(data);
+			try {
+				this.data = JSON.parse(data);
+			} catch (err) {
+				console.log("Failed to parse cache")
+				console.log(err)
+			}
 	});
-	console.log("Loading cache")
 }
 
 
--- index.js
+++ index.js
@@ -24,6 +24,59 @@ let queueInterval
 
 //[Utility]
 
+function playerWon(game) {
+	return (game.radiant_win && game.player_slot < 128) || (!game.radiant_win && game.player_slot > 127) 
+}
+
+
+//has to be improved somehow
+function dumpAliases(){
+	console.log("Dumping aliases and discordAliases")
+	fs.writeFile("aliases.json", JSON.stringify(aliases), (err) => {
+		if (err)
+			console.log(err)
+	})
+	fs.writeFile("discordAliases.json", JSON.stringify(discordAliases), (err) => {
+		if (err)
+			console.log(err)
+	})
+}
+
+function loadAliases(){
+	console.log("Loading aliases")
+	fs.readFile("aliases.json", 'utf8', function (err,data) {
+			if (err) {
+				console.log("Failed to load aliases");
+			}
+			try {
+				aliases = JSON.parse(data);
+			} catch (err) {
+				console.log("Failed to parse aliases")
+				console.log(err)
+			}
+	});
+	fs.readFile("discordAliases.json", 'utf8', function (err,data) {
+			if (err) {
+				console.log("Failed to load discordAliases");
+			}
+			try {
+				discordAliases = JSON.parse(data);
+			} catch (err) {
+				console.log("Failed to parse discordAliases")
+				console.log(err)
+			}
+	});
+}
+
+
+//художественный фильм спиздили
+String.prototype.format = function () {
+  var args = arguments;
+  return this.replace(/{([0-9]+)}/g, function (match, index) {
+    return typeof args[index] == 'undefined' ? match : args[index];
+  });
+};
+
 function getUsersAlias(userId) {
 	return discordAliases[userId] ? discordAliases[userId].alias : undefined
 }
@@ -31,6 +84,7 @@ function getUsersAlias(userId) {
 //shortcut for text
 function getPhrase(code) {
 	return phrases[code] && phrases[code][config.language] ? phrases[code][config.language] : phrases.phraseError.ru + " " + code
+	
 }
 
 //checking if account id is valid
@@ -38,22 +92,127 @@ function validAccId(id) {
 	return /^\d\d\d\d\d\d\d\d\d$/.test(id)
 }
 
-let criteriae = ["wins", "games", "winrate", "net", "gpm", "xpm", "kills", "deaths", "kda", "leaver", "damage"]
-let timelimits = ["alltime", "recent", "today"]
+let criteriae = ["wins", "games", "winrate", "gpm", "xpm", "kills", "deaths", "kda", "lasthit", "leaver", "damage", "heal", "tower"]
+
+function calcAvg(a, param) {
+	let sum = a.reduce((a,b) => {
+		let newObj = {}
+		newObj[param] = a[param] + b[param] 		
+		return newObj 
+	})[param]
+	return sum/a.length
+}
 
 //fns for getting the proper stat from WL info
 let fns = {
 	"wins" : (a) => {
-		a.mainCrit = a.json.win
+		a.data = a.data.win
+		return a
 	},
 	
 	"games": (a) => {
-		a.mainCrit = a.json.win + a.json.lose
+		a.data = a.data.win + a.data.lose
+		return a
 	},
 	
-	"winrate" : (a) => {
-		a.mainCrit = a.json.win / (a.json.lose + a.json.win) * 100
+	"winrate": (a) => {
+		a.data = a.data.win / (a.data.lose + a.data.win) * 100
+		return a
+	},
+
+	"gpm": (a) => {
+		a.data = calcAvg(a.data, "gold_per_min")
+		return a
+	},
+
+	"xpm": (a) => {
+		a.data = calcAvg(a.data, "xp_per_min")
+		return a
+	},
+
+	"kills": (a) => {
+		a.data = calcAvg(a.data, "kills")
+		return a
+	},
+
+	"deaths": (a) => {
+		a.data = calcAvg(a.data, "deaths")
+		return a
+	},
+
+	"kda": (a) => {
+		a.data = (calcAvg(a.data, "kills") + calcAvg(a.data, "assists")) / calcAvg(a.data, "deaths")
+		return a
+	},
+
+	"lasthit": (a) => {
+		a.data = calcAvg(a.data, "last_hits")
+		return a
+	}, 
+
+	"leaver": (a) => {
+		a.data = a.data.some((game) => {
+			return game.leaver_status
+		})
+		return a
+	},
+
+	"damage": (a) => {
+		a.data = calcAvg(a.data, "hero_damage")
+		return a
+	},
+
+	"heal": (a) => {
+		a.data = calcAvg(a.data, "hero_healing")
+		return a
+	}, 
+
+	"tower": (a) => {
+		a.data = calcAvg(a.data, "tower_damage")
+		return a
 	}
+}
+
+async function getLeaderboard(criteria, limit) {
+	let urlexts = []
+
+	const wlCrit = criteriae.slice(0,3).includes(criteria)
+
+	const urlExtEnding = wlCrit ? "wl" : "recentMatches" 
+
+	for(let alias of leaderboard) {
+		urlexts.push({urlext: `players/${aliases[alias]}/${urlExtEnding}`, alias: alias})
+	}
+
+	results = await fetchMany(urlexts);
+
+	results = results.map(fns[criteria]).sort((a,b) => { return b.data - a.data})
+
+	if (limit!==0) {
+		results = results.slice(0, limit)
+	}
+
+	return results
+}
+
+function stringifyLeaderboard(criteria, leaderboard) {
+	if(leaderboard.length == 0)
+		return getPhrase("noParticipants")
+	let res = criteria + "\n"
+	for(let pos of leaderboard) {
+		res += (leaderboard.indexOf(pos) + 1) + " " + pos.alias + " " + pos.data + "\n"
+	}
+	return res
+}
+
+function stringifyStats(stats) {
+	let res = "Stats for " + stats.alias + "\n"
+	for (let crit in stats) {
+		if (criteriae.includes(crit)) {
+			res += crit + ": " + stats[crit].data + "\n"
+		}
+	} 
+	return res
 }
 
 
@@ -63,6 +222,8 @@ let fns = {
 // load cache on starting the bot if cacheFilename is provided earlier
 if (config.cacheFilename)
 	cache.loadLatest()
+
+loadAliases()
 
 // start dumping cache periodically after some time
 setTimeout(() => {
@@ -90,7 +251,7 @@ async function fetch(urlextension, forceupdate=false){
 	cacheExpired = false;
 
 	if (!noCache) {
-		cacheExpired =  !cache.data[urlextension].permanent && ((Date.now() - cache.data[urlextension].timestamp) > cacheLifetime)
+		cacheExpired =  !cache.data[urlextension].permanent && ((Date.now() - cache.data[urlextension].timestamp) > config.cacheLifetime)
 		if (cacheExpired)
 			console.log("Cache expired")
 	}
@@ -99,7 +260,7 @@ async function fetch(urlextension, forceupdate=false){
 
 	if (forceupdate || noCache || cacheExpired) {
 		console.log("Requesting " + urlextension + "...")
-		let result = await axios.get(APIURL + urlextension)
+		let result = await axios.get(config.APIURL + urlextension)
 		console.log("Done " + urlextension)
 		if (result.status != 200) {
 			console.log("Fetch failed: " + result.status)
@@ -118,20 +279,23 @@ async function fetch(urlextension, forceupdate=false){
   	}
 	}
 
-	return cache[urlextension].data
+	return cache.data[urlextension].data
 }
 
-
-//kinda deprecated or smth
 async function fetchMany(urlexts) {
+	if(urlexts.length >= config.maxSimultFetch){
+		console.log("Attempted")
+	}
+
 	let promises = []
 	let results = []
 
-	console.log("Fetching ")
+	console.log("Fetching " + urlexts.length	+ " urls")
 
-	urlexts.forEach((urlext)=>{
-		promises.push(fetch(urlext).then((res)=>{
-			results.push(res)
+	urlexts.forEach((req)=>{
+		promises.push(fetch(req.urlext).then((res)=>{
+			req.data = res
+			results.push(req)
 		}))
 	})
 
@@ -142,35 +306,6 @@ async function fetchMany(urlexts) {
 	return results
 }
 
-// grabs a part of game caching queue and requests it for caching. !!! Doesnt check
-// whether a game is already cached, so that should be fixed
-
-/*
-function fetchBatchFromQueue(){
-	let batch = queue.length > maxCallsPerMinute ? queue.slice(0, maxCallsPerMinute) : queue
-
-	queue = queue.slice(batch.length)
-
-	if(queue.length === 0) {
-		setTimeout(() => {
-			queueActive = false;
-			clearInterval(queueInterval);
-		}, 60000)
-	}
-
-	console.log("Fetching " + batch.length + " games")
-
-	batch.forEach((game) => {
-		fetch("matches/"+game.match_id)
-	})
-}
-
-*/
-
-//[Getting info in advance]
-
-let heroes = fetch("heroes");
-
 let info;
 
 fs.readFile('./commands.txt', 'utf8', function (err,data) {
@@ -180,56 +315,59 @@ fs.readFile('./commands.txt', 'utf8', function (err,data) {
   info = data;
 });
 
-
 //[Message handling]
 
-function Command(pseudos, handler, enabled) {
-	this.pseudos = pseudos;
+class Command {
+	constructor(pseudos, handler, enabled) {
+		this.pseudos = pseudos;
 
-	this._handler = handler;
+		this._handler = handler;
 
-	this.enabled = enabled;
+		this.enabled = enabled;
 
-	this.fits = function(command) {
-		return this.pseudos.any((pseudo) => {
-			return pseudo == command
-		})
+		this.fits = function(command) {
+			return this.pseudos.some((pseudo) => {
+				return pseudo == command
+			})
+		}
+
+		this.handle = function(args, userId) {
+			if (!this.enabled) {
+				return getPhrase('commandDisabled')
+			} else return this._handler(args, userId);
+		}
+
+		commands.push(this)
+
+		return this
 	}
-
-	this.handle = function(args, userId) {
-		if (!this.enabled) {
-			return getPhrase('commandDisabled')
-		} else return this._handler(args, userId);
-	}
-
-	commands.push(this)
 }
 
-let handle = function(commandText, args, userId) {
+let handle = async function(commandText, args, userId) {
 	let fitting = commands.filter((command) => {
-		command.fits(commandText)
+		return command.fits(commandText)
 	})
 	if (fitting.length === 0) return getPhrase("commandNotFound")
 	//multiple found?
-	return fitting[0].handle(args)
+	return await fitting[0].handle(args, userId)
 }
 
 //actual commands
 
-Command(["commands", "help"], (args, userId)=>{
+new Command(["commands", "help"], (args, userId)=>{
 	return info;
 }, true)
 
-Command(["deletecache"], (args, userId)=>{
-	Object.entries(cache.data).forEach((urlext, data) => {
-		if(!data.permanent) {
-			cache[urlext] = undefined
-		}
-	})
+new Command(["deletecache"], (args, userId)=>{
+	for(urlext in cache.data) {
+		if(!cache.data[urlext].permanent)
+			cache.data[urlext] = undefined
+	}
+	console.log(cache.data)
 	return getPhrase("cacheCleared")
 }, true)
 
-Command(["iam"], (args, userId)=>{
+new Command(["iam"], (args, userId)=>{
 	if (args.length === 0) {
 		if (getUsersAlias(userId)) {
 			return getPhrase("yourAliasIs").format(getUsersAlias(userId))
@@ -240,11 +378,12 @@ Command(["iam"], (args, userId)=>{
 		if(!aliases[args[0]]) 
 			return getPhrase("noSuchAlias")
 		discordAliases[userId] = {alias: args[0]}
+		dumpAliases()
 		return getPhrase("yourAliasNowIs").format(args[0])
 	}
 }, true)
 
-Command(["alias", "a"], (args, userId)=> {
+new Command(["alias", "a"], (args, userId)=> {
 	if (args.length < 2) 
 		return getPhrase("seeUsage")
 	if (validAccId(args[0])) 
@@ -254,70 +393,130 @@ Command(["alias", "a"], (args, userId)=> {
 
 	aliases[args[0]] = args[1];
 
-	if(args.length == 2) 
+	if(args.length == 2) {
+		dumpAliases()
 		return getPhrase("aliasAdded").format(args[0], args[1])
-
-	if(args.length === 3) {
-		discordAliases[userId] = {alias: args[0], name: message.member.user.username}
+	}
+ 
+	if(args.length == 3 && args[2] === "iam") {
+		discordAliases[userId] = {alias: args[0]}
+		dumpAliases()
 		return getPhrase("aliasAddedWithDiscord").format(args[0], args[1])
 	}
 
-	if(args.length >= 4) {
+	if(args.length >= 4 && args[2] === "iam" && args[3] === "enter") {
+		discordAliases[userId] = {alias: args[0]}
 		leaderboard.push(args[0])
+		dumpAliases()
 		return getPhrase("aliasAddedWithDiscordAndLeaderboard").format(args[0], args[1])
-	}
+	} 
 }, true)
 
-Command(["enterleaderboard", "enter"])
+new Command(["enterleaderboard", "enter"], (args, userId) => {
+	let alias = args.length > 0 ? args[0] : getUsersAlias(userId)
+	if(typeof aliases[alias] == "undefined")
+		return getPhrase("noAlias")
+	if(leaderboard.includes(alias))
+		return getPhrase("alreadyInLeaderboard")
+	leaderboard.push(alias)
+	return getPhrase("leaderboardSuccess")
+}, true)
+
+new Command(["leaderboard", "all"], async (args, userId) => {
+	let criteria = args.length > 0 ? args[0] : criteriae[0]
+
+	if (!criteriae.includes(criteria))
+		return getPhrase("noSuchCriteria")
+
+	let results = await getLeaderboard(criteria, 0)
+
+	return stringifyLeaderboard(criteria, results)
+
+}, true)
+
+new Command(["leaders", "l"], async (args, userId) => {
+	let criteria = args.length > 0 ? args[0] : criteriae[0]
+
+	if (!criteriae.includes(criteria))
+		return getPhrase("noSuchCriteria")
+
+	let results = await getLeaderboard(criteria, 3)
+
+	return stringifyLeaderboard(criteria, results)
+
+}, true)
+
+new Command(["best"], async (args, userId) => {
+	let criteria = args.length > 0 ? args[0] : criteriae[0]
+
+	if (!criteriae.includes(criteria))
+		return getPhrase("noSuchCriteria")
+
+	let results = await getLeaderboard(criteria, 1)
+
+	return stringifyLeaderboard(criteria, results)
+
+}, true)
+
+new Command(["stats", "s"], async (args, userId) => {
+	let alias = args.length > 0 ? args[0] : getUsersAlias(userId)
+	if(typeof aliases[alias] == "undefined")
+		return getPhrase("noSuchAlias")
+	let data = await fetch(`players/${aliases[alias]}/recentMatches`)
+	let results = {alias:alias, data:data}
+	for(let crit of criteriae.slice(3)) {
+		results[crit] = {criteria: crit, data: data}
+		fns[crit](results[crit])
+	}
+	let wins = data.filter((game) => {
+		return playerWon(game)
+	}).length
+	results["wins"] = {
+		data: wins,
+		criteria: "wins"
+	}
+	results["data"] = undefined
+	return stringifyStats(results)
+}, true)
+
+new Command(["last"], async (args, userId) => {
+	let alias = args.length > 0 ? args[0] : getUsersAlias(userId)
+	if(typeof aliases[alias] == "undefined")
+		return getPhrase("noSuchAlias")
+	let data = await fetch(`players/${aliases[alias]}/recentMatches`)
+	data = data.slice(0, 1)
+	console.log(data)
+	let results = {alias:alias, data:data}
+	for(let crit of criteriae.slice(3)) {
+		results[crit] = {criteria: crit, data: data}
+		fns[crit](results[crit])
+	}
+	let wins = data.filter((game) => {
+		return playerWon(game)
+	}).length
+	results["wins"] = {
+		data: wins,
+		criteria: "wins"
+	}
+	results["data"] = undefined
+	return stringifyStats(results)
+}, true)
 
 client.on("messageCreate", async function (message) {
-
-  
-
   if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(config.prefix)) return;
 
-  const commandBody = message.content.slice(prefix.length);
+  const commandBody = message.content.slice(config.prefix.length);
   const args = commandBody.split(' ');
   const command = args.shift().toLowerCase();
 
-  message.reply(handle(command, args, message.member.user.id));
-
-  //[Network-related commands]
-
   try {
-
-
-  	if (queue.length === 0) {
-	  	if (command === "leaderboard" || command === "all") {
-	 	  	let paramIndices = getParamIndices(args[0], args[1])
-	 			getLeaderboard(paramIndices.cInd, paramIndices.tInd).then((results) => {
-	 				console.log("Replying")
-	 				console.log(results)
-	 				message.reply(JSON.stringify(results))	
-	 			}); 	  	
-	  	}
-  	}	else {
-  		message.reply("В данный момент бот прогружает игры и не может выполнять ряд команд.");
-  	}
-	} catch (error) {
-		console.log(error)
-		message.reply("Упс, что-то пошло не так. Вероятно превышено число запросов к API")
-	}
-
-  if(command === "enterleaderboard" || command === "enter") {
-  	let alias = args.length > 0 ? args[0] : getUsersAlias();
-  	if (alias == undefined) {
-  		message.reply("Привяжите алиас или укажите его в команде")
-  		return
-  	}
-  	leaderboard.push(alias)
-  	message.reply("Вы теперь участник лидерборда")
+  	handle(command, args, message.member.user.id).then((res)=>{
+			message.reply(res);
+  	})
+  } catch (err) {
+  	message.reply(err.toString())
   }
-
-
-
-
 });
 
 client.login(keyObj.secret_key)
